3f25747d264fc48cc4b3e7b6800821522c5bf37e
Jose Luis Mora <jlrazr@users.noreply.github.com>
2023-08-11 10:16:56 -0600

    avances

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,20 +1,50 @@
-import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { getCompany } from '../lib/graphql/queries'
+import JobList from '../components/JobList';
 function CompanyPage() {
   const { companyId } = useParams();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false
+  })
 
-  const company = companies.find((company) => company.id === companyId);
-  return (
-    <div>
+  useEffect(() => {
+    (async () => {
+      try {
+        const company = await getCompany(companyId)
+        setState({company, loading: false, error: false})
+      } catch (error) {
+        setState({company: null, loading: false, error: true})
+      }
+    })()
+  }, [companyId])
+
+  const {company, loading, error} = state
+
+  if (loading) {
+    return <div><p>Loading...</p></div>
+  }
+
+  else if (error) {
+    return <div><p className='has-text-danger'>An error has occurred...</p></div>
+  }
+
+  else {
+    return (
+      <div>
       <h1 className="title">
         {company.name}
       </h1>
       <div className="box">
         {company.description}
       </div>
+      <h2 className='title is-5'>Jobs at {company.name}</h2>
+      <JobList jobs={company.jobs} />
     </div>
-  );
+    );
+  }
 }
 
 export default CompanyPage;
